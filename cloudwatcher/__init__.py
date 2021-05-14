cf3aadecc05e22e679c4d8f4443b8f8d0d7661ba
TotallyFred <f.detienne@gmail.com>
2021-05-14 19:05:15 +0200

    added __init__.py

---
+++ cloudwatcher/__init__.py
@@ -0,0 +1,9 @@
+"""
+pyCloudWatcher.
+
+Native Python bindings for the LunaticoAstro's CloudWatcher weather station API.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Stephen Hudson'
+__credits__ = 'Argonne National Laboratory'
